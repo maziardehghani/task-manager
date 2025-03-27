0707b5e129f9698eef86e7a180dfd7b64a23ffbf
maziardehghani <maziardehghani1380@gmail.com>
2025-03-27 08:31:02 +0330

    add new file for api routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,11 +3,17 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )->withRouting(
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,1 @@
 <?php
-
-use App\Http\Controllers\TaskController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/tasks', [TaskController::class, 'index'] );
