51a05de0f40210ebbb46c0f30bef6a9bd9d068f7
maziardehghani <maziardehghani1380@gmail.com>
2025-03-29 17:34:15 +0330

    assert users cant see other users tasks

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -6,6 +6,7 @@ use App\Models\Task;
 use App\Models\User;
 use App\Traits\TestTools;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
@@ -34,11 +35,29 @@ class TaskTest extends TestCase
     {
         $testUser = User::query()->whereEmail('maziar@gmail.com')->first();
 
-        $task = Task::query()->whereUser($testUser)->inRandomOrder()->first()->id;
+        $task = Task::query()->whereUser($testUser)->inRandomOrder()->first()?->id;
 
         $response = $this->getData(route('tasks.show', $task));
 
         $response->assertStatus(200);
+    }
+
+    public function test_users_cant_see_other_users_tasks(): void
+    {
+        $user1 = User::factory()->create(['email' => 'user1@example.com']);
+        $user2 = User::factory()->create(['email' => 'user2@example.com']);
+
+        $user1Task = Task::factory()->create(['user_id' => $user1->id]);
+
+        $token = $user2->createToken('test-token')->plainTextToken;
+
+        $response = $this->withHeaders([
+            "Accept" => "application/json",
+            "Authorization" => 'Bearer ' . $token
+        ])->getJson(route('tasks.show', $user1Task->id));
+
+        $response->assertStatus(403);
+
     }
 
 }
